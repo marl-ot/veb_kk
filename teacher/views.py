f2aab3a4362f85f6eabac5b52b736f483a0d84fc
marl-ot <kalmykov.swyatoslaw@yandex.ru>
2022-05-18 12:03:43 +0300

    new links

--- teacher/views.py
+++ teacher/views.py
@@ -10,9 +10,10 @@ def Teacher_classes(request):
 
 #def show_class(request, num_id):
 #    if 4 < num_id < 12:
-#        return render(request, 'teacher/class.html')
-#    else:
-#        return pageNotFound(request)
+#        if request.user.is_teacher:
+#            return render(request, 'teacher/class.html')
+#        else:
+#            return pageNotFound(request)
 
 
 def show_class_a(request, num_id):
