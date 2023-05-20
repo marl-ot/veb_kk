e046b771d7beadda5fce99d37483173bf99325fa
marl-ot <kalmykov.swyatoslaw@yandex.ru>
2023-05-20 19:14:15 +0300

    Update settings.py
    del secret_key

--- veb_kk/settings.py
+++ veb_kk/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1ddj-@vh64t5t&j$#yf76m4i$8mmc#(m+(%mh)1b-cx(y2k*1k'
+SECRET_KEY = secretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
